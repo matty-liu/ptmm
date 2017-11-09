397687c2851cd90a132d0054a18fb848b9939fca
Matthew Liu <ninja2koo@hotmail.com>
2017-11-08 22:18:16 -0500

    gathered additional data

---
+++ places-manhat-data-sample3.js
@@ -0,0 +1,4 @@
+// populartimes.get("apikey", ["restaurant"], (40.726283, -74.014318), (40.747423, -73.968146))
+// 40.726283, -74.014318     |     40.747423, -73.968146
+// Holland Tunnel                  Queens Midtown Tunnel
+// too large of an api request
