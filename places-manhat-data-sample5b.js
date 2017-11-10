e66566c07143178c8090944ce6db825968a7a462
Matthew Liu <ninja2koo@hotmail.com>
2017-11-10 08:51:00 -0500

    move file into new folder

--- places-manhat-data-sample5b.js
+++
@@ -1,5 +0,0 @@
-
-// populartimes.get("AIzaSyASUnStAUyUBSLjckIAwrFwrg2e4mTnmGs", ["restaurant"], (40.750778, -74.016167), (40.753733, -73.994834))
-// 40.750778, -74.016167      |     40.753733, -73.994834
-// Pier 62                           9th ave w35th
-// ( locations)
