ce87e0b4a45ee92a4bb81a13581419bdc660d076
Matthew Liu <ninja2koo@hotmail.com>
2017-11-12 11:13:01 -0500

    added bar and nightclub data

---
+++ data/places-manhat-data-join.js
@@ -0,0 +1,8 @@
+
+let googleRestaurants = googleRestaurantsData1.concat(googleRestaurantsData2)
+googleRestaurants = googleRestaurants.concat(googleRestaurantsData3)
+googleRestaurants = googleRestaurants.concat(googleRestaurantsData4)
+googleRestaurants = googleRestaurants.concat(googleRestaurantsData5)
+googleRestaurants = googleRestaurants.concat(googleRestaurantsData6)
+let googleBars = bars1
+let googleNightclub = googleNightclub1
