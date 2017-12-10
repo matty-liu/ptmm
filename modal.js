bbc7a68ec1bb555b8073ae3f23245b4273ead22a
Matthew Liu <ninja2koo@hotmail.com>
2017-12-09 23:35:07 -0500

    add fadein and fadeout transition for modal(no jQuery)

--- modal.js
+++ modal.js
@@ -7,20 +7,22 @@ var btn = document.getElementById("infoModalButton");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// debugger
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
     modal.style.display = "flex";
+    modal.style.animation = "fadein 0.2s linear";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modal.style.animation = "fadeout 0.2s linear";
+    setTimeout( () => {modal.style.display = "none";}, 190)
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+  if (event.target == modal) {
+    modal.style.animation = "fadeout 0.2s linear";
+    setTimeout( () => {modal.style.display = "none";}, 190)
+  }
 }
