221fc3d2be9b8852c8abfdd6201af19af9814eb1
Matthew Liu <ninja2koo@hotmail.com>
2017-12-13 09:42:49 -0500

    alter fadeout transition timing for slower devices

--- modal.js
+++ modal.js
@@ -4,8 +4,8 @@ var modal = document.getElementById('infoModal');
 // Get the button that opens the modal
 var btn = document.getElementById("infoModalButton");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+// Get the <div> element that closes the modal
+var divClose = document.getElementsByClassName("close")[0];
 
 // When the user clicks on the button, open the modal
 btn.onclick = function() {
@@ -15,15 +15,15 @@ btn.onclick = function() {
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+divClose.onclick = function() {
     modal.style.animation = "fadeout 0.2s linear";
-    setTimeout( () => {modal.style.display = "none";}, 190)
+    setTimeout( () => {modal.style.display = "none";}, 175)
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.animation = "fadeout 0.2s linear";
-    setTimeout( () => {modal.style.display = "none";}, 190)
+    setTimeout( () => {modal.style.display = "none";}, 175)
   }
 }
